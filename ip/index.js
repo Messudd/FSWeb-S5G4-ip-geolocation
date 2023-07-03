3fb0d0f59aab377fa1fbbdec73de00484cf51444
Messudd <90100309+Messudd@users.noreply.github.com>
2023-07-03 17:19:29 +0300

    Mesud_S5G4_IP

--- ip/index.js
+++ ip/index.js
@@ -17,53 +17,7 @@ async function ipAdresimiAl() {
       benimIP = a;
     });
 }
-// ------------ değiştirmeyin --------------
 
-/*
-	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
-    (tag içindeki yere kendi ipnizi yazarak URL'yi oluşturun):
-    https://apis.ergineer.com/ipgeoapi/<ipniz>
-
-	NOT: Bilgisayarın IP adresini öğrenmek için: https://apis.ergineer.com/ipadresim
-	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
-*/
-
-/*
-	ADIM 2: Geri döndürülen verileri inceleyin, bu sizin ip bilgileriniz! Bileşen fonksiyonunuzu geliştirmek içindeki bu veri yapısını
-	iyice anlamanız gerekmektedir.
-
-*/
-
-/*
-	ADIM 3: Argümanı sadece 1 nesne kabül eden bir fonksiyon oluşturun.
-    DOM metotlarını ve özelliklerini kullanarak, şunları gerçekleştirin:
-
-	<div class="card">
-	<img src={ülke bayrağı url} />
-	<div class="card-info">
-		<h3 class="ip">{ip adresi}</h3>
-		<p class="ulke">{ülke bilgisi (ülke kodu)}</p>
-		<p>Enlem: {enlem} Boylam: {boylam}</p>
-		<p>Şehir: {şehir}</p>
-		<p>Saat dilimi: {saat dilimi}</p>
-		<p>Para birimi: {para birimi}</p>
-		<p>ISP: {isp}</p>
-	</div>
-    </div>
-*/
-
-/*
-	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve
-	bu kartı DOM olarak .cards elementinin içine ekleyin.
-*/
-
-/*
-	ADIM 5: Manuel olarak eklediğiniz IP adresini dinamiğe dönüştürün.
-	Sayfanın en üstünde ---değiştirmeyin--- etiketleri arasında yer alan asenkron ipAdresimiAl() fonksiyonuna
-	sorgu atarak bilgisayarınız IP adresini dinamik olarak aldıracaksınız. Bu fonksiyon asenkron olarak çağırıldığında `benimIP` değişkenine
-	bilgisayarınızın IP adresini atayacaktır.
-	Örnek dinamik URL kullanımı: var url = "https://apis.ergineer.com/ipgeoapi/"+benimIP;
-*/
 const flagTR =
   'https://tr.wikipedia.org/wiki/T%C3%BCrk_bayra%C4%9F%C4%B1#/media/Dosya:Flag_of_Turkey.svg';
 
@@ -96,14 +50,40 @@ function createNewCard(dataInfo) {
   return cardHtml;
 }
 
-async function getLocation(myIP) {
-  await setTimeout(() => {
-    axios
-      .get('https://apis.ergineer.com/ipgeoapi/188.3.216.5')
-      .then((responce) => {
-        const cardContent = createNewCard(responce.data);
-        cardParentDiv.innerHTML = cardContent;
-      });
-  }, 3000);
+let objectData = {};
+
+async function getDataObject(IP_adress) {
+  await axios
+    .get(`https://apis.ergineer.com/ipgeoapi/${IP_adress}`)
+
+    .then(function (responce) {
+      objectData = responce.data;
+      console.log(`Request Data : `, objectData);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(function () {
+      console.log('Request Data Final State : ', objectData);
+      cardParentDiv.innerHTML = createNewCard(objectData);
+    });
 }
-getLocation();
+
+async function resultCardShow() {
+  await ipAdresimiAl();
+  getDataObject(benimIP);
+}
+
+resultCardShow();
+
+// async function getLocation(myIP) {
+//   await setTimeout(() => {
+//     axios
+//       .get('https://apis.ergineer.com/ipgeoapi/188.3.216.5')
+//       .then((responce) => {
+//         const cardContent = createNewCard(responce.data);
+//         cardParentDiv.innerHTML = cardContent;
+//       });
+//   }, 3000);
+// }
+// getLocation();
